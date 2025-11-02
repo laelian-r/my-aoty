ab21b94b86924c7f28539533504851fbb5806541
laelian-r <laelian.roux@gmail.com>
2025-11-02 16:03:45 +0100

    feat: update Header component to conditionally render based on user authentication status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,54 @@
 import "../styles/Header.css";
 
+import { useAuth } from "../hooks/useAuth";
+import { getUserById } from "../utils/authentification";
+import { useEffect, useState } from "react";
+
 import Buttons from "./Buttons";
 
 export default function Header() {
-	return (
-		<>
-			<header>
-				<a href="/">
-					<h1>My AOTY</h1>
-				</a>
-				<nav>
-					<ul>
-						<li>
-							<Buttons href="/login" text="Connexion" />
-						</li>
-						<li>
-							<Buttons
-								className="sign-up"
-								href="/register"
-								text="Inscription"
-							/>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</>
-	);
+	const { user } = useAuth();
+
+	if (!user) {
+		return (
+			<>
+				<header>
+					<a href="/">
+						<h1>My AOTY</h1>
+					</a>
+					<nav>
+						<ul>
+							<li>
+								<Buttons href="/login" text="Connexion" />
+							</li>
+							<li>
+								<Buttons
+									className="sign-up"
+									href="/register"
+									text="Inscription"
+								/>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<header>
+					<a href="/">
+						<h1>My AOTY</h1>
+					</a>
+					<nav>
+						<ul>
+							<li>
+								<Buttons href="/" text="Déconnexion" className="sign-up" />
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</>
+		);
+	}
 }
